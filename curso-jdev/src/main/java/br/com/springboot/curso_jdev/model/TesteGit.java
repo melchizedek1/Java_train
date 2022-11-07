48682ddb7bb8b79cf97fe5620381f4125f285151
Melquisedeque <melquisedequesilvasantos@gmail.com>
2022-11-07 14:05:12 -0300

    Commit de teste

---
+++ curso-jdev/src/main/java/br/com/springboot/curso_jdev/model/TesteGit.java
@@ -0,0 +1,23 @@
+package br.com.springboot.curso_jdev.model;
+
+public class TesteGit {
+
+	private String nome;
+	
+	private int idade;
+	
+	public int getIdade() {
+		return idade;
+	}
+	public void setIdade(int idade) {
+		this.idade = idade;
+	}
+	public String getNome() {
+		return nome;
+	}
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+	
+	
+}
